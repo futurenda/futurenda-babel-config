7bfd5392bec78baf6e7e1efe47dbae7b8a9a3a21
fnlctrl <fnlctrl@gmail.com>
2018-08-26 13:08:01 +0800

    finish config

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # futurenda-babel-config
 Babel config used across futurenda projects
+
+## Usage
+`npm install futurenda-babel-config`
+
+Note that you can't install as devDependency as npm can't resolve `require('@babel/core') to a devDependency's dependency.
+
+Then create your babel.config.js and inside it:
+```javascript
+module.exports = require('futurenda-babel-config')(options)
+```
---
+++ config.js
@@ -0,0 +1,67 @@
+/**
+ * Usage
+ * // babel.config.js 
+ * module.exports = require('futurenda-babel-config')()
+ */
+module.exports = ({
+    debug = false,
+    targets = { android: '4.4', ios: '9' },
+    plugins = [],
+    presets = []
+} = {}) => ({
+    presets: [
+        ['@babel/preset-env', {
+            debug,
+            targets,
+            modules: false,
+            useBuiltIns: 'usage',
+            loose: true,
+            exclude: [
+                'es6.typed.data-view',
+                'es6.typed.int8-array',
+                'es6.typed.uint8-array',
+                'es6.typed.uint8-clamped-array',
+                'es6.typed.int16-array',
+                'es6.typed.uint16-array',
+                'es6.typed.int32-array',
+                'es6.typed.uint32-array',
+                'es6.typed.float32-array',
+                'es6.typed.float64-array',
+                'es6.object.set-prototype-of',
+                'es6.function.name',
+                'es6.regexp.match',
+                'es6.regexp.replace',
+                'es6.regexp.split',
+                'es6.regexp.search',
+                'web.dom.iterable'
+            ]
+        }]
+    ].concat(presets),
+    plugins: [
+        'babel-plugin-transform-string-to-import',
+
+        '@babel/plugin-syntax-jsx',
+        'babel-plugin-transform-vue-jsx',
+
+        // Stage 1
+        '@babel/plugin-proposal-export-default-from',
+        //'@babel/plugin-proposal-logical-assignment-operators',
+        ['@babel/plugin-proposal-optional-chaining', { loose: true }],
+        //['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
+        ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: true }],
+        '@babel/plugin-proposal-do-expressions',
+
+        // Stage 2
+        //['@babel/plugin-proposal-decorators', { legacy: true }],
+        //'@babel/plugin-proposal-function-sent',
+        '@babel/plugin-proposal-export-namespace-from',
+        //'@babel/plugin-proposal-numeric-separator',
+        '@babel/plugin-proposal-throw-expressions',
+
+        // Stage 3
+        '@babel/plugin-syntax-dynamic-import',
+        //'@babel/plugin-syntax-import-meta',
+        //['@babel/plugin-proposal-class-properties', { loose: false }],
+        '@babel/plugin-proposal-json-strings'
+    ].concat(plugins)
+})
